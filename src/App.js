b4b2f635a324251e7f57ffc52aefab34c032814d
programcode200 <rohans1021@gmail.com>
2025-03-08 13:44:31 +0530

    fix bugs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import ResMenu from "./components/ResMenu";
